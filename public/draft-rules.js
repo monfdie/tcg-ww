399dc9f22aa5f5f4072adddbe5d037819ead0511
monfdie <yelloweye788@gmail.com>
2026-02-12 23:14:17 +0200

    Update draft-rules.js

--- public/draft-rules.js
+++ public/draft-rules.js
@@ -30,33 +30,48 @@ const DRAFT_RULES = {
         { team: 'red', type: 'pick' }, { team: 'blue', type: 'pick' },     
         { team: 'blue', type: 'pick' }, { team: 'red', type: 'pick' }       
     ],
+    // Generals 2 (бывший Heavy Ban)
+    // 6 Банов, 3 Пика
     'generals_2': [
-        { team: 'blue', type: 'ban' }, { team: 'red', type: 'ban' },
-        { team: 'blue', type: 'ban' }, { team: 'blue', type: 'ban' },
-        { team: 'red', type: 'ban' },  { team: 'red', type: 'ban' },
-        { team: 'blue', type: 'ban' }, { team: 'blue', type: 'pick' },
-        { team: 'red', type: 'ban' },  { team: 'red', type: 'ban' },
-        { team: 'red', type: 'pick' }, { team: 'blue', type: 'ban' },
-        { team: 'blue', type: 'ban' }, { team: 'blue', type: 'pick' },
-        { team: 'red', type: 'ban' },  { team: 'red', type: 'pick' },
-        { team: 'red', type: 'pick' }, { team: 'blue', type: 'pick' }
+        { team: 'blue', type: 'ban' },
+        { team: 'red', type: 'ban' },
+        { team: 'blue', type: 'ban' },
+        { team: 'blue', type: 'ban' },
+        { team: 'red', type: 'ban' },
+        { team: 'red', type: 'ban' },
+        { team: 'blue', type: 'ban' },
+        { team: 'blue', type: 'pick' },
+        { team: 'red', type: 'ban' },
+        { team: 'red', type: 'ban' },
+        { team: 'red', type: 'pick' },
+        { team: 'blue', type: 'ban' },
+        { team: 'blue', type: 'ban' },
+        { team: 'blue', type: 'pick' },
+        { team: 'red', type: 'ban' },
+        { team: 'red', type: 'pick' },
+        { team: 'red', type: 'pick' },
+        { team: 'blue', type: 'pick' }
     ]
 };
 
-// --- СПЕЦИАЛЬНАЯ ЛОГИКА ДЛЯ GITCG CUP 2 ---
-// 1. Фаза иммунитета (4 шага)
-const IMMUNITY_PHASE = [
-    { team: 'blue', type: 'immunity_ban' },  // Запретить брать в имун
+// Генерация схемы GITCG CUP 2
+const gitcg2 = JSON.parse(JSON.stringify(DRAFT_RULES['gitcg']));
+gitcg2[13].immunity = true; 
+gitcg2[14].immunity = true;
+gitcg2[25].immunity = true; 
+gitcg2[27].immunity = true;
+DRAFT_RULES['gitcg_cup_2'] = gitcg2;
+
+const IMMUNITY_ORDER = [
+    { team: 'blue', type: 'immunity_ban' },
     { team: 'red', type: 'immunity_ban' },
-    { team: 'blue', type: 'immunity_pick' }, // Выбрать имун
+    { team: 'blue', type: 'immunity_pick' },
     { team: 'red', type: 'immunity_pick' }
 ];
 
-// 2. Объединяем: Сначала фаза иммуна, потом обычный драфт gitcg
-DRAFT_RULES['gitcg_cup_2'] = [...IMMUNITY_PHASE, ...DRAFT_RULES['gitcg']];
-
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { DRAFT_RULES };
+    module.exports = { DRAFT_RULES, IMMUNITY_ORDER };
 } else {
     window.DRAFT_RULES = DRAFT_RULES;
+    window.IMMUNITY_ORDER = IMMUNITY_ORDER;
 }
