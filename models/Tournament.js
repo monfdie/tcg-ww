ac9e1fea28e815b6b4ec245fe651325895565c48
monfdie <yelloweye788@gmail.com>
2026-02-16 23:06:15 +0200

    Update Tournament.js

--- models/Tournament.js
+++ models/Tournament.js
@@ -1,23 +1,33 @@
 const mongoose = require('mongoose');
 
 const tournamentSchema = new mongoose.Schema({
-    slug: { type: String, unique: true }, // Сделали необязательным (убрали required)
+    slug: { type: String, unique: true },
     title: String,
     date: String,
     prize: String,
     region: String,
     system: String,
-    regLink: String,
     
-    // Новые поля
+    // Ссылки
+    regLink: String,      // Регистрация
+    rulesLinkRu: String,  // Регламент RU
+    rulesLinkEn: String,  // Регламент EN
+    
+    // Внешний вид
     image: { type: String, default: null },
     type: { type: String, default: 'tournament' },
-    
-    description: { type: String, default: '' }, // Текст внутри окна
-    openInModal: { type: Boolean, default: false }, // Если true — откроет окно
-    
+    description: { type: String, default: '' },
+    openInModal: { type: Boolean, default: false },
     cardStyle: { type: String, default: 'blue' },
     badgeText: { type: String, default: 'OPEN' },
+    
+    // ЛЕНТА НОВОСТЕЙ (Как в Телеграме)
+    announcements: [{
+        text: String,
+        image: String,
+        date: { type: Date, default: Date.now }
+    }],
+
     visibleUntil: { type: Date },
     isLive: { type: Boolean, default: true }
 });
