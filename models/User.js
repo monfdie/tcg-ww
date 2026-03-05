38f925daaac62b2cd1968b54742292f2dd3666d0
monfdie <yelloweye788@gmail.com>
2026-03-05 14:49:08 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
     avatar: String,
     role: { type: String, default: 'player' }, // Можно будет выдавать админки
     gamesPlayed: { type: Number, default: 0 },
-    dateRegistered: { type: Date, default: Date.now }
+    dateRegistered: { type: Date, default: Date.now },
+    box: { type: [String], default: [] } // <--- Добавлено сохранение Бокса
 });
 
 module.exports = mongoose.model('User', userSchema);
